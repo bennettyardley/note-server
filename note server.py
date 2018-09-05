b2e07d71032e3b9f43b39fbb02dc042c80a22284
Bennett Yardley <33298178+bennettyardley@users.noreply.github.com>
2018-09-05 17:07:59 -0400

    Add files via upload

---
+++ note server.py
@@ -0,0 +1,16 @@
+from twisted.internet.protocol import Protocol, Factory
+from twisted.internet import reactor
+
+class Save(Protocol):
+    def receivedSaveText(self, saveText):
+        self.transport.write(saveText)
+
+
+def main():
+    f = Factory()
+    f.protocol = Save
+    reactor.listenTCP(1234, f)
+    reactor.run()
+
+
+main()
