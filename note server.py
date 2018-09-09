bdac4457483f328cf5de64f2aa583e37fe2a7975
Bennett Yardley <33298178+bennettyardley@users.noreply.github.com>
2018-09-08 21:08:15 -0400

    Update note server.py

--- note server.py
+++ note server.py
@@ -1,7 +1,7 @@
 from twisted.internet.protocol import Protocol, Factory
 from twisted.internet import reactor
 
- Save(Protocol):
+class Save(Protocol):
     def receivedSaveText(self, saveText):
         self.transport.write(saveText)
 
